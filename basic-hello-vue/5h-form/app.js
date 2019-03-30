fb87c17e4858ed7d6b50c01969a96e34e9d1f45d
zzl81cn <zzl81cn@163.com>
2019-03-30 17:45:02 +0800

    输入绑定

---
+++ basic-hello-vue/5h-form/app.js
@@ -0,0 +1,86 @@
+let app1 = new Vue({
+    el: '#app1',
+    data: {
+        message: ''
+    }
+});
+
+let app2 = new Vue({
+    el: '#app2',
+    data: {
+        messageText: '',
+        checkboxA: false
+    }
+});
+
+let app3 = new Vue({
+    el: '#app3',
+    data: {
+        checkedNames: []
+    }
+});
+
+let app4 = new Vue({
+    el: '#app4',
+    data: {
+        picked: ''
+    }
+});
+
+let app5 = new Vue({
+    el: '#app5',
+    data: {
+        selected: ''
+    }
+});
+
+let app6 = new Vue({
+    el: '#app6',
+    data: {
+        selected: []
+    }
+});
+
+let app7 = new Vue({
+    el: '#app7',
+    data: {
+        selected: 'A',
+        options: [
+            {text: 'One', value: 'A'},
+            {text: 'Two', value: 'B'},
+            {text: 'There', value: 'C'}
+        ]
+    }
+});
+
+let app8 = new Vue({
+    el: '#app8',
+    data: {
+        toggle: ''
+    }
+});
+
+let app9 = new Vue({
+    el: '#app9',
+    data: {
+        pick: '',
+        pre: 'a'
+    }
+});
+
+let app10 = new Vue({
+    el: '#app10',
+    data: {
+        selected: ''
+    }
+});
+
+let app11 = new Vue({
+    el: '#app11',
+    data: {
+        msg: '',
+        age: 0,
+        str: ''
+    }
+});
+
