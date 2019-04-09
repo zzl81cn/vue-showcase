33a0b5bc16088db907d509c4ced7f1ed62883ec2
zzl81cn <zzl81cn@163.com>
2019-04-09 14:36:50 +0800

    add this.$router.push({...})

--- vue-dev/src/components/MyFooter.vue
+++ vue-dev/src/components/MyFooter.vue
@@ -1,12 +1,21 @@
 <template>
     <div id="MyFooter">
       <span>Copyright &copy; <a href="">zzl81cn</a> All rights reserved</span>
+      <button @click="routerHome">home</button>
     </div>
 </template>
 
 <script>
     export default {
-        name: "MyFooter"
+        name: "MyFooter",
+        methods: {
+          /* 编程式路由 https://router.vuejs.org/zh/guide/essentials/navigation.html */
+          routerHome() {
+            this.$router.push({
+              path: '/'
+            })
+          }
+        }
     }
 </script>
 
