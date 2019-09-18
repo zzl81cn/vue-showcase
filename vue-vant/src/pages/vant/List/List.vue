83f151ef063bb0b1187dc969bf64efa322e2cb79
zzl81cn <zzl81cn@163.com>
2019-09-18 19:19:54 +0800

    dev at vant

--- vue-vant/src/pages/vant/List/List.vue
+++ vue-vant/src/pages/vant/List/List.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>van list</div>
+  <div>
+    <h5>van demo</h5>
+    <div class="button">
+      <van-button type="primary">xxx</van-button>
+      <van-button type="warning">warning</van-button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { Button } from 'vant'
 export default {
-  name: 'List'
+  name: 'List',
+  components: {
+    [Button.name]: Button
+  }
 }
 </script>
 
