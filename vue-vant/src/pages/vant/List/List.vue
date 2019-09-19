133b21f21d289967b27aaaf90dab92926d4798a0
zzl81cn <zzl81cn@163.com>
2019-09-19 15:01:38 +0800

    add baidu tongji

--- vue-vant/src/pages/vant/List/List.vue
+++ vue-vant/src/pages/vant/List/List.vue
@@ -2,7 +2,7 @@
   <div>
     <h5>van demo</h5>
     <div class="button">
-      <van-button type="primary">xxx</van-button>
+      <van-button type="primary" @click="checkHmt">xxx</van-button>
       <van-button type="warning">warning</van-button>
     </div>
   </div>
@@ -14,6 +14,15 @@ export default {
   name: 'List',
   components: {
     [Button.name]: Button
+  },
+  methods: {
+    checkHmt () {
+      if (window._hmt) {
+        console.log('list', window._hmt)
+      } else {
+        console.log('no')
+      }
+    }
   }
 }
 </script>
