9f7cc5fae4154872b03ba3dcf86bafb53cc44100
zzl81cn <zzl81cn@163.com>
2020-05-09 15:33:09 +0800

    data drop same

--- vue-show-cli2.x/src/pages/element/index.vue
+++ vue-show-cli2.x/src/pages/element/index.vue
@@ -18,6 +18,12 @@
     data () {
       return {
         allreadyData: [
+          /*{
+            "field_code": "def",
+            "field_alias": "count_def",
+            "field_name": "456",
+            "type": "count"
+          },*/
           {
             "field_code": "abc",
             "field_name": "123"
@@ -29,20 +35,22 @@
     },
     methods: {
       process() {
+        this.aPro = []
         // console.log('h')
-        let tempData = this.aTwinListData.concat(this.allreadyData)
-        for (let i=0, len = tempData.length; i<len; i++) {
+        // debugger
+        // let tempData = this.aTwinListData.concat(this.allreadyData)
+        /*for (let i=0, len = tempData.length; i<len; i++) {
           for (let j=i+1; j<len; j++) {
             if (tempData[i].hasOwnProperty('type')) {
-              console.log('te', i, tempData[i], tempData[i] == tempData[j], tempData[i].field_code, tempData[j].field_code)
-              if (tempData[i].field_alias === tempData[j].field_alias) {
+              console.log('te', i, tempData[i], tempData[i].field_alias === tempData[j].field_alias, tempData[i].field_code, tempData[j].field_code)
+              if (tempData[i].field_alias.indexOf(tempData[j].field_alias) !== -1) {
                 tempData.splice(j, 1);
                 // splice 会改变数组长度，所以要将数组长度 len 和下标 j 减一
                 len--;
                 j--;
               }
             } else {
-              if (tempData[i].field_code === tempData[j].field_code && (!tempData[j].hasOwnProperty('type'))) {
+              if (tempData[i].field_code.indexOf(tempData[j].field_code) !== -1 && (!tempData[j].hasOwnProperty('type'))) {
                 tempData.splice(j, 1);
                 // splice 会改变数组长度，所以要将数组长度 len 和下标 j 减一
                 len--;
@@ -50,8 +58,29 @@
               }
             }
           }
+        }*/
+        let aData = this.aTwinListData
+        let bData = this.allreadyData
+        for (let i = 0, alen = aData.length; i < alen; i++) {
+          for (let j = 0, blen = bData.length; j < blen; j++) {
+            if (aData[i].hasOwnProperty('type') && bData[j].hasOwnProperty('type')) {
+              console.log('if', aData[i].field_alias, bData[j].field_alias, aData[i].field_alias.indexOf(bData[j].field_alias) !== -1)
+              if (aData[i].field_alias.indexOf(bData[j].field_alias) !== -1) {
+                aData.splice(i, 1);
+                i--;
+                alen--
+              }
+            } else {
+              console.log('else', aData[i].field_code, bData[j].field_code, aData[i].field_code.indexOf(bData[j].field_code) !== -1)
+              if (aData[i].field_code.indexOf(bData[j].field_code) !== -1 && (!aData[i].hasOwnProperty('type'))) {
+                aData.splice(i, 1);
+                i--;
+                alen--
+              }
+            }
+          }
         }
-        this.aPro = tempData
+          this.aPro = aData
       }
     },
     mounted() {
