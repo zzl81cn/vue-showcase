92a4b02863c4859127daa0e53fb7a83037f2cb8f
zzl81cn <zzl81cn@163.com>
2020-07-28 22:35:27 +0800

    add singleCheckTree

--- vue-show-cli2.x/src/pages/element/index.vue
+++ vue-show-cli2.x/src/pages/element/index.vue
@@ -5,6 +5,7 @@
         <li><router-link to="/element/table">table</router-link></li>
         <li class="nav-btn"><router-link to="/element/tree">Tree</router-link></li>
         <li class="nav-btn"><router-link to="/element/tree-inline">TreeInline(rc)</router-link></li>
+        <li class="nav-btn"><router-link to="/element/tree-check-single">TreeCheckSingle</router-link></li>
       </ul>
     </header>
     <router-view></router-view>
