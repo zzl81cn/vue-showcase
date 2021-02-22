f8154bf0ea571de5d06b7100285307b3d443c429
zzl81cn <zzl81cn@163.com>
2021-02-22 18:10:34 +0800

    add vue config

--- vue-cli4-x/vue.config.js
+++ vue-cli4-x/vue.config.js
@@ -1,4 +1,56 @@
 // vue.config.js
+
+const path = require('path')
+const resolve  = dir => path.join(__dirname, dir)
+const webpack = requre('webpack')
+
 module.exports = {
   // 选项...
+  publickPath: './',
+  outputDir: 'dist',
+  lintOnSave: false,
+  chainWebpack: config => {
+    config.plugin('provide').use(webpack.ProvidePlugin, [{
+      $: 'jquery',
+      jQuery: 'jquery',
+      "windows.jQuery": 'jquery'
+    }])
+    if (process.env.NODE_ENV === 'development') {
+      config.resolve.symlinks(true)
+    } else {
+      /* product */
+    }
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+
+    } else {
+      /* dev */
+      config.devtool = 'source-map'
+    }
+  },
+  css: {},
+  devServer: {
+    open: false,
+    host: "0.0.0.0",
+    port: 8080,
+    https: false,
+    hotOnly: true,
+    overlay: {
+      warnings: false,
+      errors: false
+    },
+    /* proxy: {
+      '/xx': {
+        target: 'http://2323',
+        changeOrigin: true,
+        pathRewirte: {
+          '^/xx': 'xx'
+        }
+      }
+    } */
+    parallel: require('os').cpus().length > 1,
+    // 生产环境开启sourceMap
+    productionSourceMap: false
+  }
 }
